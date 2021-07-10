d42a09f2e88fc7b88f1a646332acb6aa8541243f
Henrique <nascimentocostahenrique@gmail.com>
2021-07-09 23:20:00 -0300

    Alterações Frontend

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div>
+      <h2>Pesquisar CNPJ</h2>
+      <div>
+        <input v-model="cnpj" type="text"><button>Pesquisar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      cnpj: ""
+    }
+  },
+  methods: {
+    requestApi: function() {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
