37217208cffe7b5208e259b192389179c8636749
Henrique <nascimentocostahenrique@gmail.com>
2021-07-11 09:46:28 -0300

    Alteracoes frontend

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,23 +3,39 @@
     <div>
       <h2>Pesquisar CNPJ</h2>
       <div>
-        <input v-model="cnpj" type="text"><button>Pesquisar</button>
+        <input v-model="cnpj" type="text"><button @click="requestApi">Pesquisar</button>
       </div>
+      <div v-if="aviso" id="aviso">
+        CNPJ Incorreto
+      </div>
+      {{resultado}}
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'Home',
   data() {
     return {
-      cnpj: ""
+      cnpj: "",
+      resultado: [],
+      aviso: null
     }
   },
   methods: {
     requestApi: function() {
-      
+      if(this.cnpj == "" || this.cnpj.length < 14 || this.cnpj.length > 14) {
+        this.aviso = true;
+      } else {
+        axios.get("http://localhost:8000/api/"+this.cnpj)
+        .then((res)=>{
+          this.resultado.push(res);
+        });
+        this.aviso = null;
+      }
     }
   }
 }
@@ -27,5 +43,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#aviso {
+  background-color: #ff8585;
+  width: 40%;
+  margin: 0 auto;
+  margin-top: 5%;
+  padding: 5px 5px;
+  border-color:#000;
+  border-radius: 3px;
+}
 </style>
